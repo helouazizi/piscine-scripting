33ad97c1ee0fb032ee534f70ae85c814ee8f91e4
helouaziz <ouazizi2code@gmail.com>
2026-03-31 12:05:35 +0100

    aaa

--- file-details.sh
+++ file-details.sh
@@ -2,33 +2,22 @@
 
 DIR="hard-perm"
 
-mkdir -p "$DIR"
+# Create structure
 mkdir -p "$DIR/0" "$DIR/3" "$DIR/A"
-touch "$DIR"/{1,2,4,5,6,7,8,9,11}
+touch "$DIR"/{1,2,4,5,6,7,8,9}
 
-# Apply correct permissions
-# bash hard-perm.sh
-# chmod 701 "$DIR/0"
-# chmod 402 "$DIR/1"
-# chmod 604 "$DIR/2"
-# chmod 777 "$DIR/3"
-# chmod 510 "$DIR/4"
-# chmod 460 "$DIR/5"
-# chmod 460 "$DIR/6"
-# chmod 510 "$DIR/7"
-# chmod 604 "$DIR/8"
-# chmod 402 "$DIR/9"
-# chmod 701 "$DIR/A"
-chmod 401 hard-perm/0
-chmod 402 hard-perm/1
-chmod 604 hard-perm/2
-chmod 777 hard-perm/3
-chmod 510 hard-perm/4
-chmod 460 hard-perm/5
-chmod 460 hard-perm/6
-chmod 510 hard-perm/7
-chmod 604 hard-perm/8
-chmod 402 hard-perm/9
-chmod 401 hard-perm/A
+# Apply standard permissions
+chmod 755 "$DIR/0"
+chmod 644 "$DIR/1"
+chmod 644 "$DIR/2"
+chmod 755 "$DIR/3"
+chmod 755 "$DIR/4"
+chmod 644 "$DIR/5"
+chmod 644 "$DIR/6"
+chmod 755 "$DIR/7"
+chmod 644 "$DIR/8"
+chmod 644 "$DIR/9"
+chmod 755 "$DIR/A"
 
-ls -l --time-style="+%Y-%m-%d %H:%M" "$DIR" | sed '1d' | awk '{print $1, $6, $7, $8, $9}'
+# Output formatted listing (including directory itself)
+ls -l --time-style="+%Y-%m-%d %H:%M" "$DIR"
