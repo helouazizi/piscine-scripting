8048e8c2ff6d6142172dba4374e5151addf5175e
helouaziz <ouazizi2code@gmail.com>
2026-03-31 11:50:03 +0100

    change

--- file-details.sh
+++ file-details.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ls -l --time-style="+%Y-%m-%d %H:%M" hard-perm | sed '1d' | awk '{print $1, $6, $7, $8, $9}'
+ls -l --time-style="+%Y-%m-%d %H:%M" "$(dirname "$0")/hard-perm" \
+| sed '1d' \
+| awk '{print $1, $6, $7, $8, $9}'
