f011102edc87f057b77849dedef42a0cc0869e63
helouaziz <ouazizi2code@gmail.com>
2026-03-31 12:16:38 +0100

    add function

--- file-details.sh
+++ file-details.sh
@@ -2,21 +2,17 @@
 
 DIR="hard-perm"
 
-mkdir -p "$DIR/0" "$DIR/3" "$DIR/A"
+mkdir -p "$DIR"/{0,3,A}
+
 touch "$DIR"/{1,2,4,5,6,7,8,9}
 
-chmod 755 "$DIR/0"
-chmod 644 "$DIR/1"
-chmod 644 "$DIR/2"
-chmod 755 "$DIR/3"
-chmod 755 "$DIR/4"
-chmod 644 "$DIR/5"
-chmod 644 "$DIR/6"
-chmod 755 "$DIR/7"
-chmod 644 "$DIR/8"
-chmod 644 "$DIR/9"
-chmod 755 "$DIR/A"
+chmod 755 "$DIR/0" "$DIR/3" "$DIR/4" "$DIR/7" "$DIR/A"
+chmod 644 "$DIR/1" "$DIR/2" "$DIR/5" "$DIR/6" "$DIR/8" "$DIR/9"
 
-ls -l --time-style="+%Y-%m-%d %H:%M" "$DIR" | sed '1d' | awk '{print $1, $6, $7, $8, $9}'
+format_ls() {
+  awk '{printf "%s %s %s %s %s\n", $1, $6, $7, $8, $9}'
+}
 
-ls -ld --time-style="+%Y-%m-%d %H:%M" "$DIR" | awk '{print $1, $6, $7, $8, $9}'
+ls -l --time-style="+%Y-%m-%d %H:%M" "$DIR" | sed '1d' | format_ls
+
+ls -ld --time-style="+%Y-%m-%d %H:%M" "$DIR" | format_ls
