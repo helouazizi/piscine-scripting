7eb9d8b94295f05ea1c6593574fe77cd9306754f
helouaziz <ouazizi2code@gmail.com>
2026-03-31 12:26:27 +0100

    refactor file permissions and update format_ls function

--- file-details.sh
+++ file-details.sh
@@ -3,16 +3,20 @@
 DIR="hard-perm"
 
 mkdir -p "$DIR"/{0,3,A}
-
 touch "$DIR"/{1,2,4,5,6,7,8,9}
 
-chmod 755 "$DIR/0" "$DIR/3" "$DIR/4" "$DIR/7" "$DIR/A"
-chmod 644 "$DIR/1" "$DIR/2" "$DIR/5" "$DIR/6" "$DIR/8" "$DIR/9"
+chmod 755 "$DIR"/{0,3,4,7,A}
+chmod 644 "$DIR"/{1,2,5,6,8,9}
 
 format_ls() {
-  awk '{sub(/[ \t]+$/, ""); print $1, $6, $7, $8, $9}'
+  awk '{
+    date=$6;
+    time=$7;
+    name=$NF;
+    perm=$1;
+    print perm, date, time, name
+  }'
 }
 
 ls -l --time-style="+%Y-%m-%d %H:%M" "$DIR" | sed '1d' | format_ls
-
 ls -ld --time-style="+%Y-%m-%d %H:%M" "$DIR" | format_ls
