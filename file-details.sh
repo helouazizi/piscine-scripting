10a3b53c461e9c318fb50b556a1025ed4e573722
helouaziz <ouazizi2code@gmail.com>
2026-03-31 12:07:17 +0100

    vvvvv

--- file-details.sh
+++ file-details.sh
@@ -2,11 +2,9 @@
 
 DIR="hard-perm"
 
-# Create structure
 mkdir -p "$DIR/0" "$DIR/3" "$DIR/A"
 touch "$DIR"/{1,2,4,5,6,7,8,9}
 
-# Apply standard permissions
 chmod 755 "$DIR/0"
 chmod 644 "$DIR/1"
 chmod 644 "$DIR/2"
@@ -19,5 +17,4 @@ chmod 644 "$DIR/8"
 chmod 644 "$DIR/9"
 chmod 755 "$DIR/A"
 
-# Output formatted listing (including directory itself)
-ls -l --time-style="+%Y-%m-%d %H:%M" "$DIR"
+ls -l --time-style="+%Y-%m-%d %H:%M" "$DIR" | sed '1d' | awk '{print $1, $6, $7, $8, $9}'
