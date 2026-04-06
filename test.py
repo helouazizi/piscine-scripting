e53b34cfd9ac498ddf4033d85d2320e9f2e9d867
helouaziz <ouazizi2code@gmail.com>
2026-04-06 14:12:41 +0100

    feat: add numerical operations module with modulo, divide, and integer division functions

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-import punishment
+import numerical_operations_the_return
 
-print(punishment.do_punishment(' ', ' ', 4), end='')
+print(numerical_operations_the_return.modulo(10, 3))          # 1
+print(numerical_operations_the_return.divide(10, 3))          # 3.3333333333333335
+print(numerical_operations_the_return.divide(10, 0))          # 0
+print(numerical_operations_the_return.integer_division(10, 3))# 3
