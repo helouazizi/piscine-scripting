40181664c4a2db7f5ef8d964516c88c7a830d27c
helouazizi <ouazizi2code@gmail.com>
2026-04-13 14:00:41 +0100

    xxxx

--- clean_the_list.py
+++ clean_the_list.py
@@ -1,17 +1,12 @@
-def clean_list(items):
-    if len(items) == 0:
-        return []
+def clean_list(shopping_list):
+    
+    for i in range(len(shopping_list)):
+        shopping_list[i] = shopping_list[i].strip().capitalize()
 
-    has_milk = any(item.strip().lower() == 'milk' for item in items)
+    if ("Milk" not in shopping_list) and shopping_list:
+        shopping_list.append("Milk")
 
-    if not has_milk:
-        items.append('milk')
+    for i in range(len(shopping_list)):
+        shopping_list[i] = f"{i + 1}/ {shopping_list[i]}"
 
-    cleaned = []
-
-    for index, item in enumerate(items, start=1):
-        cleaned_item = item.strip().capitalize()
-        formatted = f"{index}/ {cleaned_item}"
-        cleaned.append(formatted)
-
-    return cleaned
+    return shopping_list
